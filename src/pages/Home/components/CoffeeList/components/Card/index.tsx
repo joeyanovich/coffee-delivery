14fdff05acdf774459519b98017179ae3dcc9f09
joeyanovich <joedison.dias@gmail.com>
2023-10-16 21:43:15 -0300

    working 🚧: work in progress for card

--- src/pages/Home/components/CoffeeList/components/Card/index.tsx
+++ src/pages/Home/components/CoffeeList/components/Card/index.tsx
@@ -1,9 +1,32 @@
 import { CardContainer } from "./styles";
 
+import  americano  from '../../../../../../../public/americano.png'
+// import  arabe  from '../../../../../../../public/arabe.png'
+// import  cafeComLeite  from '../../../../../../../public/cafeComLeite.png'
+// import  cafeGelado  from '../../../../../../../public/cafeGelado.png'
+// import  capuccino  from '../../../../../../../public/capuccino.png'
+// import  chocolateQuente  from '../../../../../../../public/chocolateQuente.png'
+// import  cubano  from '../../../../../../../public/cubano.png'
+// import  expressoCremoso  from '../../../../../../../public/expressoCremoso.png'
+// import  expresso  from '../../../../../../../public/expresso.png'
+// import  havaiano  from '../../../../../../../public/havaiano.png'
+// import  irlandes  from '../../../../../../../public/irlandes.png'
+// import  latte  from '../../../../../../../public/explatteresso.png'
+
 export function Card() {
   return (
     <CardContainer>
-      Cards
+      <div className="img">
+        <img src={americano} alt="" />
+      </div>
+      <div className="tag">
+        <span>TRADICIONAL</span>
+      </div>
+      <h3>Expresso Tradicional</h3>
+      <span>O tradicional café feito com água quente e grãos moídos</span>
+      <footer>
+        <input type="number" />
+      </footer>
     </CardContainer>
   )
 }
--- src/pages/Home/components/CoffeeList/components/Card/styles.ts
+++ src/pages/Home/components/CoffeeList/components/Card/styles.ts
@@ -1,5 +1,39 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
+  width: 16rem;
+  height: 19.375rem;
+
+  border-radius: 6px 36px 6px 36px;
+
+  background-color: ${props => props.theme['base-card']};
   
+  .img {
+    display: flex;
+    justify-content: center;
+  }
+  img {
+    margin: -1.25rem auto 0;
+  }
+  .tag {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 0.25rem;
+
+    margin-top: 0.75rem;
+
+  }
+  .tag span {
+    font-size: 0.625rem;
+    font-weight: bold;
+
+    padding: 0.25rem 0.5rem;
+
+    border-radius: 100px;
+
+    background-color: ${props => props.theme['yellow-light']};
+    color: ${props => props.theme['yellow-dark']};
+  }
 `
