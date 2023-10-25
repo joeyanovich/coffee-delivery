8eb443b416b1c94d1bc39e981a6bf7beb4277d36
joeyanovich <joedison.dias@gmail.com>
2023-10-24 22:03:30 -0300

    work 🚧: building part of the checkout page

--- src/pages/Checkout/components/CartSelector/index.tsx
+++ src/pages/Checkout/components/CartSelector/index.tsx
@@ -5,7 +5,27 @@ export function CartSelector() {
   return (
     <CartSelectotContainer>
       <h2>Complete seu pedido</h2>
-      <ItemCart />
+      <div className="container">
+        <div className="items-info">
+          <ItemCart />
+          <div className="line" />
+        </div>
+        <div className="price-info">
+          <div className="items-total">
+            <p>Total de itens</p>
+            <span>R$ 29,70</span>
+          </div>
+          <div className="delivery">
+            <p>Entrega</p>
+            <span>R$ 3,50</span>
+          </div>
+          <div className="total">
+            <p>Total</p>
+            <span>R$ 33,20</span>
+          </div>
+        </div>
+        <button>Confirmar Pedido</button>
+      </div>
     </CartSelectotContainer>
   )
 }
--- src/pages/Checkout/components/CartSelector/styles.ts
+++ src/pages/Checkout/components/CartSelector/styles.ts
@@ -1,7 +1,58 @@
 import styled from "styled-components";
 
 export const CartSelectotContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
   width: 28rem;
 
-  background-color: yellow;
+  h2 {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.125rem;
+
+    color: ${props => props.theme['base-subtitle']};
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1.5rem;
+
+    padding: 2.5rem;
+
+    background-color: ${props => props.theme['base-card']};
+  }
+  .line {
+    width: 23rem;
+    height: 1px;
+
+    margin-top: 1.5rem;
+
+    background-color:${props => props.theme['base-button']};
+  }
+  .price-info {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+  }
+  .items-total, .delivery, .total {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .items-total p, .delivery p {
+    font-size: 0.875rem;
+  }
+  .total {
+    font-size: 1.25rem;
+    font-weight: bold;
+  }
+  button {
+    width: 100%;
+    padding: 0.75rem;
+    cursor: pointer;
+    border-radius: none;
+
+    background-color: ${props => props.theme['yellow']};
+  }
 `
