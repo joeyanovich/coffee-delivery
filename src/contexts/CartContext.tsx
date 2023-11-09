8e5ee46b9a6792f8f01b315029916716e921685f
joeyanovich <joedison.dias@gmail.com>
2023-11-08 22:15:19 -0300

    feat 🧑‍💻: creating context

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,5 +1,26 @@
-// import { createContext } from "react";
+import { createContext, ReactNode, useState } from "react";
+import { Coffees } from "../pages/Home/components/CoffeeList/components/Card";
 
-// export interface CartItem extends Card
+export interface CartItem extends Coffees {
+  quantity: number;
+}
 
-// export const CartContext = createContext({} as )
+interface CartContextType {
+  cartItems: CartItem[]
+}
+
+interface CartContextProviderProps {
+  children: ReactNode
+}
+
+export const CartContext = createContext({} as CartContextType)
+
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  return (
+    <CartContext.Provider value={{ cartItems }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
