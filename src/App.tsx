d78dd99ce72e946e6e46493ed8e9e2406c3c7315
joeyanovich <joedison.dias@gmail.com>
2023-11-08 22:23:00 -0300

    add ➕: immer installed

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
 import { Router } from './Router';
+import { CartContextProvider } from './contexts/CartContext';
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CartContextProvider>
+          <Router />
+        </CartContextProvider>
         <GlobalStyles />
       </BrowserRouter>
     </ThemeProvider>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
 import { Banner } from "./components/Banner/index";
 import { CoffeeList } from "./components/CoffeeList";
 import { HomeContainer } from "./styles";
+import { CartContext } from '../../contexts/CartContext';
 
 export function Home() {
+  useContext(CartContext)
+
   return (
     <HomeContainer>
       <Banner />
